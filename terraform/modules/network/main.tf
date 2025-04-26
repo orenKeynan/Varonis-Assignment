4bd9f501f22618012b34d60a2863ab31d6124ddf
Oren Keynan <orenkeynan@gmail.com>
2025-04-26 15:21:00 +0300

    using private ip instead of

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -14,3 +14,16 @@ resource "azurerm_subnet" "this" {
   # default = no endpoints for this subnet
   service_endpoints = lookup(var.subnet_service_endpoints, each.key, [])
 }
+
+resource "azurerm_private_dns_zone" "aca_internal" {
+  name                = "azurecontainerapps.io" # Or "privatelink.azurecontainerapps.io" etc.
+  resource_group_name = var.resource_group_name # Use the same RG as your VNet and App Gateway
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "aca_link" {
+  name                  = "${azurerm_private_dns_zone.aca_internal.name}-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.aca_internal.name
+  virtual_network_id    = azurerm_virtual_network.this.id
+  registration_enabled  = true
+}
